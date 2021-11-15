603d83106ffe9a1d6038d5faf141fc3eb5f7fc91
Amartya Sen <sen@oakland.edu>
2021-11-14 21:54:26 -0500

    demo on static websites using express

---
+++ customers.js
@@ -0,0 +1,22 @@
+const customers = [
+  {
+    id: 1,
+    name: "hector",
+    email: "hector@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "tango",
+    email: "tango@yahoo.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "charlie",
+    email: "charlie@outlook.com",
+    status: "active",
+  },
+];
+
+module.exports = customers;
--- index.js
+++ index.js
@@ -1,22 +1,92 @@
 /**
  * document can be found on: http://expressjs.com/en/4x/api.html
- * For the purpose of simple tutorial, Postman service is used to mimic HTTP client requests 
+ * For the purpose of simple tutorial, Postman service is used to mimic HTTP client requests
  * Download Postman from: https://www.postman.com/
- * * * Sign up with free account and choose role as student 
- *  
+ * * * Sign up with free account and choose role as student
+ * *** download the desktop app: https://www.postman.com/downloads/
+ * *** This will allow us to test our backend without having to build a full frontend
+ * *** once client is install use the web browser interface as normal *
  */
 
+/*********************************************
+ * IMPORT MODULES AND INITIALIZE EXPRESS
+ * *******************************************
+ */
 
-// npm install express 
-const express = require('express');
+// npm install express
+const express = require("express");
 
-// Initialize express framework 
+// other modules
+const path = require("path");
+
+// Initialize express framework
 const app = express();
 
-// Create "ENDPOINTS" a.k.a. ROUTE HANDLERS
-app.get('/', (req,res) => {
-    res.send('Hello World from Express.js');    
+/*******************************************
+ * CREATE ROUTE HANDLERS EXAMPLE
+ * *****************************************
+ */
+
+// Create "ENDPOINTS" a.k.a. ROUTE HANDLERS -- EXAMPLE 1
+// app.get("/", (req, res) => {
+//   //   res.send("<h1> Hello World from Express.js </h1>");
+//   /**
+//    * If you want to send a file which is a HTML page, use the code below
+//    * However, this is not the best way, because you will have to create a route for every html page
+//    */
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+/********************************************************
+ * EXAMPLE 2 -- Creating a Static Website in express.js
+ * ******************************************************
+ * In this example we'll make the public folder static
+ * That is, the purpose will be to serve html, css, and multi-media etc.
+ * We will incorporate the method .use() which is used to create custom middleware
+ * Making this folder static, you can add any amount of html and css in this folder and you do not have to create routes for them
+ * compare this to what you did in node.js using .createServer() of the http module.
+ * * Note -- you generally are not going to use express for static servers.
+ * It is best suited to create JSON APIs or render templates to create dynamic web apps
+ */
+app.use(express.static(path.join(__dirname, "public")));
+
+/*****************************************************************************
+ * EXAMPLE -3 : CREATING ROUTES FOR A SIMPLE RESTful API USING HARDCODED DATA
+ * ***************************************************************************
+ */
+/**
+ * create some hardcoded array of objects in a separate file customers.js
+ * Set it up for export
+ * Import it here
+ */
+const customers = require("./customers");
+
+/**
+ * ./Create the route handle to return the above array as JSON
+ * We will call this route using POSTMAN with a GET request
+ */
+app.get("/api/customers", (req, res) => {
+  res.json(customers);
 });
 
-// Start server on a PORT 
-app.listen(3000, ()=> console.log(`Server running on 3000`));
+/*******************************************************************************************
+ * EXAMPLE -4: CREATE A YOUR OWN MIDDLEWARE FUNCTION
+ * *******************************************************************************************
+ */
+// create middleware. @params: req, req, next (next is to point to the next function in the middleware stack)
+const logger = (req, res, next) => {
+  //log the protocol type and the date of request
+  // for date, install a 3rd party package called moment
+  console.log(`${req.protocol}://${req.get("host")}${req.originalURL}}`);
+};
+// initialize your middleware
+app.use(logger);
+
+/*********************************************************************************************
+ * START YOUR SERVER
+ * *******************************************************************************************
+ */
+// Start server on a PORT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
